47fc137f91cc4c11c27d198359a77c5b4bdc2e9f
Noah Overcash <me@ncovercash.dev>
2020-05-08 08:49:04 -0400

    Add typings file for typescript projects

---
+++ temperature.d.ts
@@ -0,0 +1,42 @@
+/**
+ * Convert Celsius to Fahrenheit
+ *
+ * @param celsius number
+ * @returns {number}
+ */
+export declare function celsiusToFahrenheit(celsius: number): number;
+/**
+ * Convert Celsius to Kelvin
+ *
+ * @param celsius number
+ * @returns {*}
+ */
+export declare function celsiusToKelvin(celsius: number): number;
+/**
+ * Convert Fahrenheit to Celsius
+ *
+ * @param fahrenheit number
+ * @returns {number}
+ */
+export declare function fahrenheitToCelsius(fahrenheit: number): number;
+/**
+ * Convert Fahrenheit to Kelvin
+ *
+ * @param fahrenheit number
+ * @returns {number}
+ */
+export declare function fahrenheitToKelvin(fahrenheit: number): number;
+/**
+ * Convert Kelvin to Celsius
+ *
+ * @param kelvin number
+ * @returns {number}
+ */
+export declare function kelvinToCelsius(kelvin: number): number;
+/**
+ * Convert Kelvin to Fahrenheit
+ *
+ * @param kelvin number
+ * @returns {number}
+ */
+export declare function kelvinToFahrenheit(kelvin: number): number;
